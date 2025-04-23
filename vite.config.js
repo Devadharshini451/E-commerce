03ceef7532268400abc4820c480ffcc56a2f64f0
Devadharshini451 <devadharshini4516@gmail.com>
2025-04-23 20:43:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/E-commerce/', // 👈 This is what tells Vite to use the correct path
   plugins: [react()],
 })
